117b979c2cbd9739dfbfc066a34cc939ad1d29b2
aman221 <as562770@gmail.com>
2022-05-29 21:02:27 +0530

    added materialize CSS and practices program again

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,37 @@
-const isPalindrome = (e) => {
-    var letters = [];
-    var word='coc'
-    var rword = "";
+document.querySelector('#palindromeForm').addEventListener('submit', (e)=> {
+    e.preventDefault();
+    let letter = [];
+    let word = document.querySelector('#userInput').value;
+    let card = document.querySelector('.card-content');
+    let p = document.createElement('p');
+    let reversUserInput = '';
 
-    for(var i = 0; i < word.length; i++){
-        letters.push(word[i]);
+    for(var i = 0;i< word.length;i++){
+        letter.push(word[i]);
     }
 
-    for(var i=0; i < word.length; i++){
-        rword += letters.pop();
+    for(var i = 0;i< word.length;i++){
+        reversUserInput += letter.pop();
     }
 
-    if(rword === word){
-        console.log("It is a Palindrome");
+    if(reversUserInput === word){
+        p.innerText = 'Yes, It is a Palindrome';
+        p.classList.add('success')
+        card.appendChild(p)
+        document.querySelector('#userInput').value = ''
+        setTimeout(() => {
+            p.innerText= ''
+        },1000)
     }
+
     else{
-        console.log('Not A Palindrome');
+        p.innerText = 'It is not a palindrome !, Try Again';
+        p.classList.add('fail')
+        card.appendChild(p)
+        document.querySelector('#userInput').value = ''
+        setTimeout(() => {
+            p.innerText= ''
+        },1000)
     }
-}
+
+})
