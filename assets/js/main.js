e8055fbd61a86d6086366aff8795bd29d7753335
aman221 <as562770@gmail.com>
2022-05-30 16:24:17 +0530

    added stack and set

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,3 +35,137 @@ document.querySelector('#palindromeForm').addEventListener('submit', (e)=> {
     }
 
 })
+
+//Stack 
+
+var Stack = function(){
+    this.count = 0;
+    this.storage = [];
+
+    this.size = function(){
+        return this.count;
+    }
+
+    this.push = function(value){
+       
+       this.storage[this.count] = value;
+       this.count++;
+       return value
+    }
+
+    this.pop = function(){
+        if(this.count === 0){
+            return 'undefined'
+        }
+        this.count--;
+        var result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result
+    }
+
+    this.peek = function(){
+        var result = this.storage[this.count-1];
+        return result;
+    }
+}
+
+var myStack = new Stack();
+
+// console.log('Initial Size of Stack : ',myStack.size());
+// console.log(myStack.push(1))
+// console.log(myStack.push('Aman'))
+// console.log('Size of Stack : ',myStack.size());
+// console.log('last element in the stack is : ',myStack.peek());
+// console.log('removing last element from the stack : ',myStack.pop());
+// console.log('Size of Stack : ',myStack.size());\
+
+// import React from 'react'
+
+function mySet(){
+    this.collection = [];
+
+    this.has = function(value){
+        return this.collection.indexOf(value) !== -1;
+    }
+
+    this.add = function(value){
+        if(!this.has(value)){
+            this.collection.push(value);
+            return true
+        }
+        return false
+    }
+
+    this.remove = function(value){
+        if(this.has(value)){
+            var index = this.collection.indexOf(value);
+            this.collection.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
+
+    this.size = function(){
+        return this.collection.length
+    }
+
+    this.values = function(){
+        return this.collection
+    }
+
+    this.union = function(otherSet){
+        var firstSet = otherSet;
+        var unionSet = new Set();
+        this.collection.forEach(element => {
+            return unionSet.add(element);
+        });
+
+        firstSet.forEach(element => {
+            return unionSet.add(element);
+        });
+        return unionSet;
+    }
+
+    this.intersection = function(otherSet){
+        var firstSet = this.collection;
+        var intersection = new Set();
+        firstSet.forEach(element => {
+            if(otherSet.has(element)){
+                return intersection.add(element)
+            }
+        });
+        return intersection;
+    }
+
+    this.difference = function(otherSet){
+        var firstSet = this.collection;
+        var difference = new Set();
+        firstSet.forEach(element => {
+            if(!otherSet.has(element)){
+                return difference.add(element)
+            }
+        });
+        return difference;
+    }
+
+    this.subset = function(otherSet){
+        var firstSet = this.collection;
+        return firstSet.every(value => otherSet.has(value));
+    }
+}
+
+var myNewSet = new mySet();
+var interSectionSet = new mySet([1,2,3,5]);
+console.log(myNewSet.size());
+console.log(myNewSet.add(1));
+console.log(myNewSet.add('asman','Aman'));
+console.log(myNewSet.add('Aman'));
+console.log(myNewSet.add(2));
+
+console.log(myNewSet.size());
+console.log(myNewSet.values());
+// console.log(myNewSet.remove('asman'));
+console.log('Union :',myNewSet.union([1,2,4,'a']));
+console.log('Intersection :',myNewSet.intersection(new Set([1,2,4,5])));
+console.log('Difference :',myNewSet.difference(new Set([1,2,4,5])));
+console.log('is Subset :',myNewSet.subset(new Set([1,2,4,5,'asman','Aman'])));
